2ac649f1e6b7f04b316e9d704623aeabcddde1ad
Marshal Miller <30907840+marshalmiller@users.noreply.github.com>
2023-02-13 16:11:26 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
+#include "box.h"
 #include <iostream>
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+  int tile1, tile2, tile3;
+  Box box;
+
+  cout << box.str() << endl;
+
+  do {
+    cout << "Enter 3 tiles: ";
+    cin >> tile1 >> tile2 >> tile3;
+    if (box.close(tile1, tile2, tile3))
+      cout << box.str() << " (" << box.score() << ")\n";
+    else
+      cout << "Invalid tile selection\n";
+  } while (box.score() && (tile1 || tile2 || tile3));
 }
