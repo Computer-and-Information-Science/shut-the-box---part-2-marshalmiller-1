345a9799bf9a4ccaf2b72eb3b2de0927a7a44e6d
Marshal Miller <30907840+marshalmiller@users.noreply.github.com>
2023-02-13 16:10:51 -0500

    Update box.h

--- box.h
+++ box.h
@@ -1,1 +1,26 @@
+#pragma once
+
+#include <string>
+
+// Class Box represents the box of tiles. Initially all 9 tiles are up.
+class Box {
+private:
+	// The tiles 1 through 9, index 0 is ignored.
+	bool tiles[10];
+public:
+
+	// Constructor, initializes the box
+	Box ();
+
+	// Close one, two, or three tiles, but only if each parameter is an "up"
+	//	tile or zero. Returns false if parameters given are not valid. If
+	//	parameters are valid, closes tiles and returns true.
+	bool close (int t1, int t2, int t3);
+
+	// Compute the score of the box. This is the sum of all "up" tiles.
+	int score () const;
+
+	// Generate a string representation of the box.
+	std::string str () const;
+};
 
